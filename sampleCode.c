af777d65db4a45a96309458e928f707cc0cb9927
xiaohua <wxh113@163.com>
2018-10-08 22:12:44 +0800

    统计1的个数

--- sampleCode.c
+++ sampleCode.c
@@ -1,1 +1,12 @@
-#define bufAlign(ptr, align)    ((ptr + (align - 1)) & ~(align - 1))
+1，地址对齐
+#define bufAlign(ptr, align)    ((ptr + (align - 1)) & ~(align - 1))  
+
+2，统计二进制1的个数
+
+while(x)
+{
+	count++;
+	X=X&(X-1);
+}
+
+
